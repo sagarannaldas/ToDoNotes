ed8e573cf384d0364bf05d27d00ff7c1fecee70f
sagar <annal7057@gmail.com>
2021-02-25 12:33:58 +0530

    Android Fast Networking get request

--- app/src/main/java/in/techrebounce/todonotes/view/BlogActivity.kt
+++ app/src/main/java/in/techrebounce/todonotes/view/BlogActivity.kt
@@ -1,16 +1,53 @@
 package `in`.techrebounce.todonotes.view
 
 import `in`.techrebounce.todonotes.R
+import `in`.techrebounce.todonotes.adapter.BlogAdapter
+import `in`.techrebounce.todonotes.model.Blog
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.ParsedRequestListener
+
 
 class BlogActivity : AppCompatActivity() {
+    private val TAG = "BlogActivity"
     lateinit var recyclerViewBlogs: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blog)
         bindViews()
+        getBlogs()
+    }
+
+    private fun getBlogs() {
+        AndroidNetworking.get("http://www.mocky.io/v2/5926ce9d11000096006ccb30")
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsObject(Blog::class.java, object : ParsedRequestListener<Blog> {
+                    override fun onResponse(response: Blog?) {
+                        setupRecyclerView(response)
+                    }
+
+                    override fun onError(anError: ANError?) {
+                        Log.d(TAG, "onError: ${anError?.localizedMessage}")
+                    }
+
+                })
+
+    }
+
+    private fun setupRecyclerView(response: Blog?) {
+        val blogAdapter = BlogAdapter(response!!.data)
+        val linearLayoutManager = LinearLayoutManager(this@BlogActivity)
+        linearLayoutManager.orientation = RecyclerView.VERTICAL
+        recyclerViewBlogs.layoutManager = linearLayoutManager
+        recyclerViewBlogs.adapter = blogAdapter
+
     }
 
     private fun bindViews() {
